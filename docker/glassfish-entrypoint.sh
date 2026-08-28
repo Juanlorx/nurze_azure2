7f7cf8682b1f361c5af9e002e887038dbadd8df5
jeyb747 <samueldiaz.g2008@gmail.com>
2026-08-27 21:53:35 -0500

    Fix GlassFish root deployment on Azure

--- docker/glassfish-entrypoint.sh
+++ docker/glassfish-entrypoint.sh
@@ -3,9 +3,14 @@ set -euo pipefail
 
 ASADMIN="${GLASSFISH_HOME}/bin/asadmin"
 
-# Se despliega en la raíz para que la URL pública de Azure sea simplemente /.
+# Azure Container Apps publica el puerto tan pronto como GlassFish inicia. Por
+# eso la aplicación debe quedar registrada antes del arranque en primer plano:
+# de otro modo se muestra la página predeterminada de GlassFish.
 "${ASADMIN}" start-domain
-"${ASADMIN}" deploy --force=true --contextroot / "${APP_WAR}"
+"${ASADMIN}" deploy --force=true --name nurse --contextroot / "${APP_WAR}"
+"${ASADMIN}" set \
+  configs.config.server-config.http-service.virtual-server.server.default-web-module=nurse
+"${ASADMIN}" list-applications | grep -q '^nurse'
 "${ASADMIN}" stop-domain
 
 exec "${ASADMIN}" start-domain --verbose
