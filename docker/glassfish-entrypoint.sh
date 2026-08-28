caa82eecde04166cb960341c9d049d0275336d89
jeyb747 <samueldiaz.g2008@gmail.com>
2026-08-27 22:15:23 -0500

    Use Java CA trust store for Azure MySQL

--- docker/glassfish-entrypoint.sh
+++ docker/glassfish-entrypoint.sh
@@ -10,6 +10,9 @@ ASADMIN="${GLASSFISH_HOME}/bin/asadmin"
 "${ASADMIN}" deploy --force=true --name nurse --contextroot /nurse "${APP_WAR}"
 "${ASADMIN}" set \
   configs.config.server-config.http-service.virtual-server.server.default-web-module=nurse
+"${ASADMIN}" create-jvm-options \
+  "-Djavax.net.ssl.trustStore=/opt/java/openjdk/lib/security/cacerts" \
+  "-Djavax.net.ssl.trustStorePassword=changeit"
 "${ASADMIN}" list-applications | grep -q '^nurse'
 "${ASADMIN}" stop-domain
 
