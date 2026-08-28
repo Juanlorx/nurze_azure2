b74d4059085d52ee29e007d58cc3f524c08ddbc8
jeyb747 <samueldiaz.g2008@gmail.com>
2026-08-27 22:06:30 -0500

    Serve Nurse as GlassFish default web module

--- docker/glassfish-entrypoint.sh
+++ docker/glassfish-entrypoint.sh
@@ -7,7 +7,7 @@ ASADMIN="${GLASSFISH_HOME}/bin/asadmin"
 # eso la aplicación debe quedar registrada antes del arranque en primer plano:
 # de otro modo se muestra la página predeterminada de GlassFish.
 "${ASADMIN}" start-domain
-"${ASADMIN}" deploy --force=true --name nurse --contextroot / "${APP_WAR}"
+"${ASADMIN}" deploy --force=true --name nurse --contextroot /nurse "${APP_WAR}"
 "${ASADMIN}" set \
   configs.config.server-config.http-service.virtual-server.server.default-web-module=nurse
 "${ASADMIN}" list-applications | grep -q '^nurse'
